4ce12be93af5f415798579439cd4c8634604c2a5
jumana chawand <101575492+Jumanacha@users.noreply.github.com>
2022-06-13 19:12:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+
+function beginPath()
+ctx.beginPath();
+ctx.strokeStyle="grey";
+ctx.lineWidth = 1;
+ctx.rect()(x, y, width, height);
+ctx.stroke();
+
+
+function beginPath()
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth= 5;
+ctx.arc()(x, y, r, startAngle, endAngle);
+ctx.stroke();
+
